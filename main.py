6ad372138e4b7eb82ee06f5d076cd60a3e3b5116
BitBeans <75768166+bitofbeans@users.noreply.github.com>
2022-01-25 21:07:16 +0000

    Added main gui with player input

---
+++ main.py
@@ -0,0 +1,151 @@
+# import pygame for gui
+import pygame as pg
+# import sudoku solver/checker (by me)
+import solver as s
+import pprint as pp
+# add board
+board = [
+    [4,2,0,  9,8,0,  5,1,0],
+    [5,0,7,  4,0,3,  0,8,9],
+    [8,0,0,  0,0,6,  4,0,7],
+    
+    [0,3,5,  8,7,0,  6,0,0],
+    [6,0,4,  0,0,0,  0,7,0],
+    [7,0,0,  1,0,4,  0,0,0],
+   
+    [3,0,0,  5,9,0,  1,0,0],
+    [0,0,0,  0,4,1,  3,2,0],
+    [2,4,1,  0,3,0,  7,9,0]
+]
+# program will use 'board' variable
+solvedBoard = [
+    [4,2,0,  9,8,0,  5,1,0],
+    [5,0,7,  4,0,3,  0,8,9],
+    [8,0,0,  0,0,6,  4,0,7],
+    
+    [0,3,5,  8,7,0,  6,0,0],
+    [6,0,4,  0,0,0,  0,7,0],
+    [7,0,0,  1,0,4,  0,0,0],
+   
+    [3,0,0,  5,9,0,  1,0,0],
+    [0,0,0,  0,4,1,  3,2,0],
+    [2,4,1,  0,3,0,  7,9,0]
+]
+# create solver sprite
+solver = s.Solver([])
+solvedBoard = solver.solve(solvedBoard)
+# begin pygame
+pg.init()
+# open window
+SIZE = 50
+SCREEN_WIDTH = 450 + 9*2
+SCREEN_HEIGHT = 450 + SIZE + 9*2
+screen = pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pg.display.set_caption('Sudoku')
+# set tps
+clock = pg.time.Clock()
+fps = 60
+# set font
+font = pg.font.SysFont('freesansbold', 40)
+# functions ---------------
+# draw text
+def drawText(text, font, color, x, y):
+    img = font.render(text, True, color)
+    screen.blit(img, (x, y))
+
+def renderBoard():
+    # RENDER SQUARES AND LINES
+    global squares
+    global squareCoords
+    squares = []
+    squareCoords = []
+    for y in range(9):
+      for x in range(9):
+        posx = x*SIZE + x*2
+        posy = y*SIZE + y*2
+        square = pg.rect.Rect(posx, posy, SIZE, SIZE)
+        squares.append(square)
+        squareCoords.append((x,y))
+        # draw squares
+        pg.draw.rect(screen,(200,200,200),square)
+        if x == 3 or x == 6:
+          # vertical lines
+          pg.draw.line(screen,(0,0,0),(posx-1,0),(posx-1,468),4)
+        if not board[y][x] == 0:
+          drawText(str(board[y][x]), font, (50,50,50),posx+5,posy+5)
+      if y == 3 or y == 6:
+        # horizontal lines
+        pg.draw.line(screen,(0,0,0),(0,posy-1),(SCREEN_WIDTH,posy-1),4)
+    # draw bottom part
+    bottom = pg.rect.Rect(0,460,SCREEN_WIDTH,SIZE)
+    pg.draw.rect(screen,(255,255,255),bottom)
+    pg.draw.line(screen,(100,100,100),(0,460),(SCREEN_WIDTH,460),4)
+    return squares, squareCoords
+
+def boardLogic(board, solvedBoard):
+  global selected
+  square = squares[selected]
+  pg.draw.rect(screen,(255,0,0),square,width = 5, border_radius=4) 
+  # get selected from click
+  mouse = pg.mouse.get_pos()
+  i = 0
+  for square in squares:
+    if square.collidepoint(mouse):
+      if pg.mouse.get_pressed()[0] == True:
+        selected = i
+    i += 1
+  # change from player in
+  keys = pg.key.get_pressed()
+  coord = squareCoords[selected]
+  x,y = coord
+  def tryKey(key):
+    if solvedBoard[y][x] == key:
+      board[y][x] = key   
+    else:
+      pass
+
+  if keys[pg.K_1]:
+    tryKey(1)
+  if keys[pg.K_2]:
+    tryKey(2)
+  if keys[pg.K_3]:
+    tryKey(3)
+  if keys[pg.K_4]:
+    tryKey(4)
+  if keys[pg.K_5]:
+    tryKey(5)
+  if keys[pg.K_6]:
+    tryKey(6)
+  if keys[pg.K_7]:
+    tryKey(7)
+  if keys[pg.K_8]:
+    tryKey(8)
+  if keys[pg.K_9]:
+    tryKey(9)
+
+  return board, solvedBoard
+
+global selected
+selected = 0
+
+# game loop
+run = True
+while run:
+    # tick clock
+    clock.tick(fps)
+    # escape condition
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+
+    # LOGIC
+     #render
+    screen.fill((255,255,255))
+    renderBoard()
+    board, solvedBoard = boardLogic(board, solvedBoard)
+
+    # update screen
+    pg.display.update()
+
+
+pg.quit()
---
+++ solver.py
@@ -0,0 +1,128 @@
+# Imports
+import pprint as pp # for output
+from collections import Counter # for more efficient counting
+from time import sleep # for adding delay
+
+# SOLVER CLASS for modularity
+class Solver():
+  def __init__(self, board_in):
+      self.map = board_in
+  
+  # solve function
+  def solve(self, board_in):
+      self.map = board_in
+      # get list of all 0's in list
+      idxList = []
+      for y in range(len(self.map)):
+          for x in range(len(self.map)):
+              row = self.map[y]
+              if row[x] == 0:
+                  idxList.append([x,y])
+      # start loop
+      solved = False
+      idx = 0
+      while not solved:
+
+          # get coords
+          x,y = idxList[idx]
+          
+          # if number is too high
+          if self.map[y][x]+1 > 9:
+              # reset number, go backwards
+              self.map[y][x] = 0
+              idx -=1
+              continue
+          # increment number
+          self.map[y][x] +=1
+          number = self.map[y][x]
+          # check constraints
+          rowcheck = checkRow(self.map, self.map[y][x], y)
+          colcheck = checkColumn(self.map, self.map[y][x], x)
+          box = int(x/3) + int(y/3) # get box position
+          boxcheck = checkBox(self.map, self.map[y][x], box)
+          if rowcheck and colcheck and boxcheck:
+              # if valid, move forward
+              idx+= 1
+              if idx > len(idxList)-1:
+                  solved = True
+          
+          sleep(0)
+      return self.map
+                  
+
+  def checkBoard(self):
+        # get list of numbers in list
+      idxList = []
+      for y in range(len(self.map)):
+          for x in range(len(self.map)):
+              idxList.append([x,y])  
+      idx = 0
+      for i in range(81):
+        x,y = idxList[idx]
+        # check
+        rowcheck = checkRow(self.map, self.map[y][x], y)
+        colcheck = checkColumn(self.map, self.map[y][x], x)
+        box = int(x/3) + int(y/3) # get box position
+        boxcheck = checkBox(self.map, self.map[y][x], box)
+        if rowcheck and colcheck and boxcheck:
+            # if valid, move forward
+            idx+= 1
+        else: 
+          return False
+      return True
+
+def checkRow(board_in, num, row_idx):
+    # get row list
+    row = board_in[row_idx]
+    # get dictionary of counts of numbers
+    count = Counter(row)
+    # get count of num in row
+    count = count[num]
+    # if not valid,
+    if count > 1:
+        # return not valid
+        return False
+    else:
+        # return valid
+        return True
+
+def checkColumn(board_in, num, col_idx):
+    # get column list
+    col = []
+    for i in range(9):
+        row = board_in[i]
+        col.append(row[col_idx])
+    # get dictionary of counts of numbers
+    counter = Counter(col)
+    # get count of num in row
+    count = counter[num]
+    # if not valid,
+    if count > 1:
+        # return not valid
+        return False
+    else:
+        # return valid
+        return True
+
+def checkBox(board_in, num, box_idx):
+    # get box coords
+    box_x = (box_idx % 3)*3
+    box_y = int(box_idx/3)*3
+    # get box list
+    box = []
+    for y in range(3):
+        for x in range(3):
+            row = board_in[box_y + y]
+            box.append(row[box_x+x])
+    # get dictionary of counts of numbers
+    count = Counter(box)
+    # get count of num in box
+    count = count[num]
+    # if not valid,
+    if count > 1:
+        # return not valid
+        return False
+    else:
+        # return valid
+        return True
+
