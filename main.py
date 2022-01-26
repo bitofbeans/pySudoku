98f80ba5c8c9c8b6dd3d92ad8f66c1211e8a7259
BitBeans <75768166+bitofbeans@users.noreply.github.com>
2022-01-26 21:37:04 +0000

    Cleaned code and moved to a class

--- main.py
+++ main.py
@@ -23,12 +23,8 @@ unsolved = [
 ]
 # add board
 board = copy.deepcopy(unsolved)
-# program will use 'board' variable
-solvedBoard = copy.deepcopy(unsolved)
-# create solver sprite
-solver = s.Solver([])
-# solve alt board
-solvedBoard = solver.solve(solvedBoard)
+
+
 
 # ---------------------------------- #
 
@@ -45,7 +41,7 @@ pg.display.set_caption('Sudoku')
 # set tps
 clock = pg.time.Clock()
 fps = 60
-
+5
 # set font
 font = pg.font.SysFont("segoeuisemilight", 35)
 fontSmall = pg.font.SysFont("segoeuisemilight", 25)
@@ -74,15 +70,33 @@ def drawText(text, font, color, x, y, align='c'):
     # draw
     screen.blit(img, rect)
 # ---------------------------------- #
+class Sudoku():
+  def __init__(self, board_in):
+    # get boards for play
+    #board to modify
+    self.board = copy.deepcopy(board_in)
+    # default unsolved board
+    self.unsolvedBoard = copy.deepcopy(board_in)
+    # default solved board
+    self.solvedBoard = copy.deepcopy(board_in)
 
-def renderBoard():
+    # get solver
+    solver = s.Solver([])
+    # solve alt board
+    self.solvedBoard = solver.solve(board_in)
+    self.squares = []
+    self.squareCoords = []
+    self.win = 0
+    self.selected = 0
+    self.pressed = 0
+    self.strikes = 0
+
+
+  def renderBoard(self):
     # RENDER SQUARES AND LINES
-    
     # use global lists
-    global squares
-    global squareCoords
-    squares = []
-    squareCoords = []
+    self.squares = []
+    self.squareCoords = []
     
     # render grid
     for y in range(9):
@@ -93,8 +107,8 @@ def renderBoard():
         
         # make list of all squares in order
         square = pg.rect.Rect(posx, posy, SIZE, SIZE)
-        squares.append(square)
-        squareCoords.append((x,y))
+        self.squares.append(square)
+        self.squareCoords.append((x,y))
         
         # draw squares
         pg.draw.rect(screen,WHITE,square,border_radius=2)
@@ -103,9 +117,9 @@ def renderBoard():
           # vertical lines 
           pg.draw.line(screen,DGREY,(posx-1,0),(posx-1,468),3)
         
-        if not board[y][x] == 0:
+        if not self.board[y][x] == 0:
           # draw text if not a 0
-          drawText(str(board[y][x]), font, FONT, posx+25, posy+25)
+          drawText(str(self.board[y][x]), font, FONT, posx+25, posy+25)
       
       if y == 3 or y == 6:
         # horizontal lines
@@ -115,150 +129,123 @@ def renderBoard():
     bottom = pg.rect.Rect(0,468,SCREEN_WIDTH,SIZE)
     pg.draw.rect(screen,WHITE,bottom)
     pg.draw.line(screen,DGREY,(0,468),(SCREEN_WIDTH,468),3)
-    drawText(f"Remaining Spots: {s.countNum(board,0)}",fontSmall, FONT, SCREEN_WIDTH/2, 493, align='lc')
-    drawText("X "*strikes, fontSmallThick, (194,0,42), 50, 494, align='c')
-    if win != 0:
+    drawText(f"Remaining Spots: {s.countNum(self.board,0)}",fontSmall, FONT, SCREEN_WIDTH/2, 493, align='lc')
+    drawText("X "*self.strikes, fontSmallThick, (194,0,42), 50, 494, align='c')
+    if self.win != 0:
       fill = pg.Surface((1000,750))  
       fill.set_alpha(200)                
       fill.fill(WHITE)           
       screen.blit(fill, (0,0))
-      if win == 1:
+      if self.win == 1:
         drawText("You Won!", font, FONT, SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50)
         drawText("Press Space to continue...", font, FONT, SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50)
-      elif win == -1:
+      elif self.win == -1:
         drawText("You Lost!", font, FONT, SCREEN_WIDTH/2, SCREEN_HEIGHT/2-50)
         drawText("Press Space to restart...", font, FONT, SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50)
+
+  def boardLogic(self):
+    # use global selected variable
+
+    # get square rect of selected
+    square = self.squares[self.selected]
+    
+    # render cursor
+    # get coord of selected square
+    coord = self.squareCoords[self.selected]
+    x,y = coord
+    
+    if self.win == 0:
+      if self.board[y][x] == 0:
+        # yellow thing
+        pg.draw.rect(screen,(200,160,5),square,width = 5, border_radius=10)
+        pg.draw.rect(screen,(249,215,28),square,width = 3, border_radius=10) 
+      else:
+        # green thing
+        pg.draw.rect(screen,(0,150,0),square,width = 5, border_radius=10)
+        pg.draw.rect(screen,(0,200,0),square,width = 3, border_radius=10) 
       
+    # get selected from click
+    mouse = pg.mouse.get_pos()
+    i = 0
+    for square in self.squares:
+      if square.collidepoint(mouse):
+        self.selected = i
+        if pg.mouse.get_pressed()[0] == True:
+          pass
+      i += 1
+
+    # keys pressed
+    keys = pg.key.get_pressed()
     
-# ---------------------------------- #
-
-def boardLogic(board, solvedBoard):
-  # use global selected variable
-  global selected
-  global win
-  global pressed
-  global strikes
-  
-  # get square rect of selected
-  square = squares[selected]
-  
-  # render cursor
-   # get coord of selected square
-  coord = squareCoords[selected]
-  x,y = coord
-  
-  if win == 0:
-    if board[y][x] == 0:
-      # yellow thing
-      pg.draw.rect(screen,(200,160,5),square,width = 5, border_radius=10)
-      pg.draw.rect(screen,(249,215,28),square,width = 3, border_radius=10) 
-    else:
-      # green thing
-      pg.draw.rect(screen,(0,150,0),square,width = 5, border_radius=10)
-      pg.draw.rect(screen,(0,200,0),square,width = 3, border_radius=10) 
+    # get coords from selected
+    coord = self.squareCoords[self.selected]
+    x,y = coord
     
-  # get selected from click
-  mouse = pg.mouse.get_pos()
-  i = 0
-  for square in squares:
-    if square.collidepoint(mouse):
-      selected = i
-      if pg.mouse.get_pressed()[0] == True:
-        pass
-    i += 1
+    # see if key works
+    def tryKey(key):
+      if self.board[y][x] != 0:
+        return
+      if self.solvedBoard[y][x] == key:
+        self.board[y][x] = key   
+      else:
+        square = self.squares[self.selected]
+        pg.draw.rect(screen,(100,0,0),square,width = 0, border_radius=10)
+        pg.draw.rect(screen,(200,0,0),square,width = 5, border_radius=10) 
 
-  # keys pressed
-  keys = pg.key.get_pressed()
-  
-  # get coords from selected
-  coord = squareCoords[selected]
-  x,y = coord
-  
-  # see if key works
-  def tryKey(key):
-    if board[y][x] != 0:
-      return
-    if solvedBoard[y][x] == key:
-      board[y][x] = key   
+        if self.pressed != key:
+          self.pressed = key
+          self.strikes += 1
+    
+    # inputs
+    if keys[pg.K_1]:
+      tryKey(1)
+    elif keys[pg.K_2]:
+      tryKey(2)
+    elif keys[pg.K_3]:
+      tryKey(3)
+    elif keys[pg.K_4]:
+      tryKey(4)
+    elif keys[pg.K_5]:
+      tryKey(5)
+    elif keys[pg.K_6]:
+      tryKey(6)
+    elif keys[pg.K_7]:
+      tryKey(7)
+    elif keys[pg.K_8]:
+      tryKey(8)
+    elif keys[pg.K_9]:
+      tryKey(9)
+    elif keys[pg.K_p]:
+      # auto solve
+      self.board = copy.deepcopy(self.solvedBoard)
     else:
-      square = squares[selected]
-      pg.draw.rect(screen,(100,0,0),square,width = 0, border_radius=10)
-      pg.draw.rect(screen,(200,0,0),square,width = 5, border_radius=10) 
-      global pressed
-      global strikes
-      if pressed != key:
-        pressed = key
-        strikes += 1
-  
-  # inputs
-  if keys[pg.K_1]:
-    tryKey(1)
-  elif keys[pg.K_2]:
-    tryKey(2)
-  elif keys[pg.K_3]:
-    tryKey(3)
-  elif keys[pg.K_4]:
-    tryKey(4)
-  elif keys[pg.K_5]:
-    tryKey(5)
-  elif keys[pg.K_6]:
-    tryKey(6)
-  elif keys[pg.K_7]:
-    tryKey(7)
-  elif keys[pg.K_8]:
-    tryKey(8)
-  elif keys[pg.K_9]:
-    tryKey(9)
-  elif keys[pg.K_p]:
-    # auto solve
-    board = copy.deepcopy(solvedBoard)
-  else:
-    pressed = 0
-  
-  if win == 1:
-    if keys[pg.K_SPACE]:
-      # restart on space
-      win = 0
-      board = copy.deepcopy(unsolved)
-      strikes = 0
-  if win == -1:
-    if keys[pg.K_SPACE]:
-      # restart on space
-      win = 0
-      board = copy.deepcopy(unsolved)
-      strikes = 0
-  
-  if board == solvedBoard:
-    # if won
-    win = 1 
-  if strikes >= 3:
-    # if lost
-    win = -1
-  
-  # return board changes
-  return board, solvedBoard
+      self.pressed = 0
+    
+    if self.win == 1:
+      if keys[pg.K_SPACE]:
+        # restart on space
+        self.win = 0
+        self.board = copy.deepcopy(self.unsolvedBoard)
+        self.strikes = 0
+    if self.win == -1:
+      if keys[pg.K_SPACE]:
+        # restart on space
+        self.win = 0
+        self.board = copy.deepcopy(self.unsolvedBoard)
+        self.strikes = 0
+    
+    if self.board == self.solvedBoard:
+      # if won
+      self.win = 1 
+    if self.strikes >= 3:
+      # if lost
+      self.win = -1
 
 # ---------------------------------- #
 
-# globals for my sanity
-## i know globals can be bad practice, 
-## but feeding all of these values to 
-## each function is just stupid,
-## and classes would be be clunky
-## forgive me :)
-global selected
-selected = 0
-global win
-win = 0 
-global strikes
-strikes = 0
-global pressed
-pressed = 0
-
-# ---------------------------------- #
 
 # game loop
-
+sudoku = Sudoku(board)
 run = True
 
 while run:
@@ -274,9 +261,8 @@ while run:
     #render
     screen.fill(LGREY)
     
-    renderBoard()
-    
-    board, solvedBoard = boardLogic(board, solvedBoard)
+    sudoku.renderBoard()
+    sudoku.boardLogic()
     
     # update screen
     pg.display.update()
